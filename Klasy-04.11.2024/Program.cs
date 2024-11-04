d16249217cf6638dabf88b7c61f5688c675fb778
Kuba Król <kuba.krol.ldz@gmail.com>
2024-11-04 12:02:18 +0100

    .

---
+++ Klasy-04.11.2024/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasy_04._11._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
