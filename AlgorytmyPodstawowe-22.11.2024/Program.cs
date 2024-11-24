32cedcbc120bc7d2022390880f984e0496799b94
Kuba <kuba.krol.ldz@gmail.com>
2024-11-24 18:56:19 +0100

    .

--- AlgorytmyPodstawowe-22.11.2024/Program.cs
+++ AlgorytmyPodstawowe-22.11.2024/Program.cs
@@ -1,101 +1,109 @@
-﻿using System.Diagnostics;
-using System.Security.Cryptography;
-using System.Text;
-using System.Xml.Linq;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace AlgorytmyPodstawowe_22._11._2024
+﻿namespace AlgorytmyPodstawowe_22._11._2024
 {
     internal class Program
     {
-        //Zasada działania algorytmu:
-        //1. Jeśli liczba podana jest mniejsza niż 2, nie jest ona liczbą pierwszą
-        //2. Jeżeli liczba jest większa, bądź równa 2, algorytm sprawdza, czy dana liczba dzieli się bez
-        //reszty przez jakąkolwiek liczbę w zakresie od 2 do pierwiastka kwadratowego z tej
-        //liczby.Jeśli znajdzie taki dzielnik, liczba nie jest pierwsza.Jeśli nie znajdzie żadnego
-        //dzielnika, oznacza to, że liczba jest pierwsza.
-        static void Zad1(int a)
-        {
+        //  Zasada działania algorytmu:
+        //  1. Jeśli liczba podana jest mniejsza niż 2, nie jest ona liczbą pierwszą
+        //  2. Jeżeli liczba jest większa, bądź równa 2, algorytm sprawdza, czy dana liczba dzieli się bez
+        //  reszty przez jakąkolwiek liczbę w zakresie od 2 do pierwiastka kwadratowego z tej
+        //  liczby.Jeśli znajdzie taki dzielnik, liczba nie jest pierwsza.Jeśli nie znajdzie żadnego
+        //  dzielnika, oznacza to, że liczba jest pierwsza.
 
-            void LiczbaPierwsza(int a)
+
+
+        // Funkcja nic nie zwraca oraz przyjmuje parametr n o typie int
+        static Action<int> Zad1 = (n) => {
+
+            Console.WriteLine($"Zadanie 1:\n");
+
+            if(n < 2)
             {
+                Console.WriteLine("nie jest ona liczbą pierwszą\n");
+            }
 
-                bool Pierwsza = true;
-
-                if(a < 2)
+            for(int i = 2;  i < n; i++)
+            {
+                if (n % i == 0)
                 {
-                    Pierwsza = false;
-                }
-
-                for(int i = 2; i < a; i++)
-                {
-                    if(a % i == 0)
-                    {
-                        Pierwsza = false;
-                        break;
-                    }
-
-                }
-
-
-
-                if(Pierwsza)
-                {
-                    Console.WriteLine($"{a} Jest to liczba pierwsza");
-                } else
-                {
-                    Console.WriteLine($"{a} Nie jest to liczba pierwsza");
+                    Console.WriteLine($"{n} nie jest liczbą pierwszą.\n");
+                    return;
                 }
             }
 
-            LiczbaPierwsza(a);
-        }
-        //Zasada działania algorytmu:
-        //1. Wejście: Dwie liczby całkowite a oraz b
-        //2. Sprawdzanie: Dopóki b nie jest równe zero:
-        //1. Obliczamy resztę z dzielenia a przez b
-        //2. Przypisujemy wartość b do a, a resztę z dzielenia tych liczb do b
-        //3. Wyjście: Kiedy b stanie się równe 0, liczba a jest największym wspólnym dzielnikiem.
-        static void Zad2(int a, int b)
-        {
-            while(b != 0)
-            {  
-                    int r = a % b;
-                    a = b;
-                    b = r;
+            Console.WriteLine($"{n} jest liczbą pierwszą\n");
+        };
 
-                if(b == 0)
-                {
-                    Console.WriteLine($"liczba {a} jest największym wspólnym dzielnikiem ");
-                    break;
-                }
-            }
-        }
-        //Zasada działania algorytmu:
-        //1. Wejście: Tekst do zaszyfrowania i klucz(liczba), który określa, o ile miejsc w alfabecie
-        //przesuniemy litery.
-        //2. Przesunięcie liter: Dla każdej litery w tekście, zamieniamy ją na literę, która jest przesunięta
-        //o wartość klucza w alfabecie.
-        //3. Obsługa końców alfabetu: Jeśli przesunięcie wykracza poza 'Z', zaczynamy od początku
-        //alfabetu ('A').
-        static void Zad3(string tekst, int klucz)
-        {
+        //  Zasada działania algorytmu:
+        //  1. Wejście: Dwie liczby całkowite a oraz b
+        //  2. Sprawdzanie: Dopóki b nie jest równe zero:
+        //  1. Obliczamy resztę z dzielenia a przez b
+        //  2. Przypisujemy wartość b do a, a resztę z dzielenia tych liczb do b
+        //  3. Wyjście: Kiedy b stanie się równe 0, liczba a jest największym wspólnym dzielnikiem.
 
-        }
-        //Zasada działania algorytmu:
-        //1. Rozpocznij od dwóch pierwszych elementów
-        //2. Aby obliczyć kolejne elementy, użyj wzoru wypisanego wyżej
-        //3. Program przestaje liczyć dalej, kiedy osiągnie element n, który został podany przez
-        //użytkownika.
-        static void Zad4(int a)
+        static Action<int, int> Zad2 = (a, b) =>
         {
-            int Fibo(int a)
+            Console.WriteLine($"Zadanie 2:\n");
+
+            while (b != 0)
             {
-                if(a == 0)
-                    return 0;
-                if(a == 1)
-                    return 1;
+                int r = a % b;
+                a = b;
+                b = r;
+            }
 
+            if (b == 0) Console.WriteLine($"liczba {a} jest największym wspólnym dzielnikiem\n");
+        };
+
+        //  Zasada działania algorytmu:
+        //  1. Wejście: Tekst do zaszyfrowania i klucz(liczba), który określa, o ile miejsc w alfabecie
+        //  przesuniemy litery.
+        //  2. Przesunięcie liter: Dla każdej litery w tekście, zamieniamy ją na literę, która jest przesunięta
+        //  o wartość klucza w alfabecie.
+        //  3. Obsługa końców alfabetu: Jeśli przesunięcie wykracza poza 'Z', zaczynamy od początku
+        //  alfabetu ('A').
+
+        static Action<string, int> Zad3 = (tekst, klucz) =>
+        {
+            Console.WriteLine($"Zadanie 3:\n");
+
+            string txt = "";
+
+
+            // Zwraca char, przyjmuje parametry typu char z i int klucz
+            Func<char, int, char> przesunZnak = (z, klucz) =>
+            {
+                if (char.IsUpper(z))
+                    // Przesunięcie w alfabecie, uwzględniając końce alfabetu
+                    return (char)((((z - 'A') + klucz) % 26) + 'A');
+                else if (char.IsLower(z))
+                    return (char)((((z - 'a') + klucz) % 26) + 'a');
+                else
+                    return z;
+            };
+
+            foreach(char z in tekst)
+            {
+                txt += przesunZnak(z, klucz);
+            }
+
+            Console.WriteLine($"Zaszyfrowany tekst: {txt}\n");
+        };
+
+
+        //  Zasada działania algorytmu:
+        //  1. Rozpocznij od dwóch pierwszych elementów
+        //  2. Aby obliczyć kolejne elementy, użyj wzoru wypisanego wyżej
+        //  3. Program przestaje liczyć dalej, kiedy osiągnie element n, który został podany przez
+        //  użytkownika.
+
+        static Action<int> Zad4 = (a) =>
+        {
+            Console.WriteLine($"Zadanie 4:\n");
+
+            static int Fibo(int a)
+            {
+                if (a == 0) return 0;
+                if (a == 1) return 1;
                 return Fibo(a - 1) + Fibo(a - 2);
             }
 
@@ -105,97 +113,202 @@ namespace AlgorytmyPodstawowe_22._11._2024
             }
 
             Console.WriteLine();
-        }
-        //Zasada działania algorytmu:
-        //1. Wejście: Liczba n, którą chcemy rozłożyć na czynniki pierwsze.
-        //2. Algorytm dzieli n przez najmniejsze liczby pierwsze, zaczynając od 2, aż liczba n stanie się
-        //równa 1. Aby zwiększyć efektywność, możemy sprawdzać podzielność tylko do pierwiastka
-        //kwadratowego z liczby, ponieważ jeśli liczba ma dzielnik większy od swojego pierwiastka,
-        //to drugi dzielnik musi być mniejszy.
-        static void Zad5(int n)
+
+        };
+
+
+        //  Zasada działania algorytmu:
+        //  1. Wejście: Liczba n, którą chcemy rozłożyć na czynniki pierwsze.
+        //  2. Algorytm dzieli n przez najmniejsze liczby pierwsze, zaczynając od 2, aż liczba n stanie się
+        //  równa 1. Aby zwiększyć efektywność, możemy sprawdzać podzielność tylko do pierwiastka
+        //  kwadratowego z liczby, ponieważ jeśli liczba ma dzielnik większy od swojego pierwiastka,
+        //  to drugi dzielnik musi być mniejszy.
+
+        static Action<int> Zad5 = (n) =>
         {
-                for(int i = 2;  i < n; i++)
+
+            Console.WriteLine($"\nZadanie 5:\n");
+
+            Console.WriteLine($"Rozkład liczby {n} na czynniki pierwsze:");
+
+            while (n % 2 == 0)
+            {
+                Console.Write($"{2} ");
+                n /= 2;
+            }
+
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                while (n % i == 0)
                 {
-                    if(n % i == 0)
-                        n = n / i;
-                        Console.Write($"{n} ");
+                    Console.Write($"{i} "); 
+                    n /= i; 
                 }
-        }
+            }
+
+            if (n > 1) Console.Write($"{n}"); // Wypisujemy ostatni czynnik
+
+            Console.WriteLine();
+        };
+
+
         //  Zasada działania algorytmu:
         //  1. Zakładamy, że pierwszy element zbioru jest zarówno najmniejszy, jak i największy.
         //  2. Dla każdego elementu porównujemy go z bieżącym najmniejszym i największym
         //  elementem.Jeśli jest mniejszy od bieżącego najmniejszego, aktualizujemy najmniejszy
         //  element.Jeśli jest większy od bieżącego największego, aktualizujemy największy element.
         //  3. Po przetworzeniu wszystkich elementów zwracamy znalezione wartości.
-        static void Zad6()
-        {
 
-        }
-        //Zasada działania algorytmu
-        //1. Algorytm przechodzi przez zbiór elementów i porównuje każdą parę sąsiadujących
-        //elementów.
-        //2. Jeśli element na lewej pozycji jest większy niż element na prawej pozycji, to zamienia je
-        //miejscami.
-        //3. Każda iteracja kończy się "wypchnięciem" największego(lub najmniejszego, w zależności
-        //od porządku) elementu na odpowiednie miejsce.Kolejne iteracje poruszają się coraz krócej,
-        //ponieważ największe elementy "osiadają" na końcu zbioru.
-        //4. Proces jest powtarzany aż do momentu, gdy w trakcie jednej pełnej iteracji nie dojdzie do
-        //żadnej zamiany.
-        static int[] Zad7(int[] n)
+        static Action<int[]> Zad6 = (n) =>
         {
-            int a = n.Length;
+            Console.WriteLine($"\nZadanie 6:\n");
 
-            for(int i = 0; i < a - 1; i++)
+            int min = n[0];
+            int max = n[0];
+
+            foreach (int number in n)
             {
-                for(int j = 0; j < a - i - 1; j++) {
-                    if (n[j] < n[j + 1])
+                if(number < min)
+                    min = number;
+                else if(number > max)
+                    max = number;
+            }
+
+            Console.WriteLine($"Najmniejszy element: {min}\n");
+            Console.WriteLine($"Największy element: {max}");
+        };
+
+        //  Zasada działania algorytmu
+        //  1. Algorytm przechodzi przez zbiór elementów i porównuje każdą parę sąsiadujących
+        //  elementów.
+        //  2. Jeśli element na lewej pozycji jest większy niż element na prawej pozycji, to zamienia je
+        //  miejscami.
+        //  3. Każda iteracja kończy się "wypchnięciem" największego(lub najmniejszego, w zależności
+        //  od porządku) elementu na odpowiednie miejsce.Kolejne iteracje poruszają się coraz krócej,
+        //  ponieważ największe elementy "osiadają" na końcu zbioru.
+        //  4. Proces jest powtarzany aż do momentu, gdy w trakcie jednej pełnej iteracji nie dojdzie do
+        //  żadnej zamiany.
+
+        static Action<int[]> Zad7 = (n) =>
+        {
+            Console.WriteLine($"\nZadanie 7:\n");
+
+            int t = n.Length;
+
+            for (int i = 0; i < t - 1; i++)
+            {
+                for (int j = 0; j < t - i - 1; j++)
+                {
+                    if (n[j] > n[j + 1])
                     {
-                        int t = n[j];
+
+                        int d = n[j];
                         n[j] = n[j + 1];
-                        n[j + 1] = t;
+                        n[j + 1] = d;
+
                     }
                 }
             }
 
-            return n;
-        }
-        //Zasada działania algorytmu:
-        //1. Algorytm przechodzi przez zbiór elementów i znajduje najmniejszy element.
-        //2. Po znalezieniu najmniejszego elementu, zamienia go z pierwszym elementem w zbiorze.
-        //3. Proces jest powtarzany dla pozostałej części zbioru (pomijając już posortowane elementy),
-        //czyli algorytm ponownie szuka najmniejszego elementu w pozostałych elementach i
-        //zamienia go z następnym w kolejności miejscem.
-        //4. Algorytm kończy pracę, gdy wszystkie elementy zostaną posortowane.
-        //NIE STOSUJEMY ŻADNYCH METOD SORTUJĄCYCH ELEMENTY W TABLICY!
-        static void Zad8() { }
-        //Zasada działania algorytmu:
-        //1. Lista elementów jest podzielona na dwie części – posortowaną i nieposortowaną.Na
-        //początku posortowana część składa się tylko z pierwszego elementu.
-        //2. Algorytm bierze pierwszy element z nieposortowanej części i wstawia go w odpowiednie
-        //miejsce w posortowanej części.
-        //3. Powtarza ten proces dla każdego kolejnego elementu z nieposortowanej części, aż cała lista
-        //zostanie posortowana.
-        //4. Proces kończy się, gdy wszystkie elementy znajdują się w posortowanej części.
-        //NIE STOSUJEMY ŻADNYCH METOD SORTUJĄCYCH ELEMENTY W TABLICY!
-        static void Zad9()
-        {
+            Console.WriteLine(string.Join(", ", n));
+        };
 
-        }
+        //  Zasada działania algorytmu:
+        //  1. Algorytm przechodzi przez zbiór elementów i znajduje najmniejszy element.
+        //  2. Po znalezieniu najmniejszego elementu, zamienia go z pierwszym elementem w zbiorze.
+        //  3. Proces jest powtarzany dla pozostałej części zbioru (pomijając już posortowane elementy),
+        //  czyli algorytm ponownie szuka najmniejszego elementu w pozostałych elementach i
+        //  zamienia go z następnym w kolejności miejscem.
+        //  4. Algorytm kończy pracę, gdy wszystkie elementy zostaną posortowane.
+        //  NIE STOSUJEMY ŻADNYCH METOD SORTUJĄCYCH ELEMENTY W TABLICY!
+
+        static Action<int[]> Zad8 = (n) =>
+        {
+            Console.WriteLine($"\nZadanie 8:\n");
+
+            int t = n.Length;
+
+            for (int i = 0; i < t - 1; i++)
+            {
+                int m = i;
+
+                // Szukamy najmniejszy element
+                for (int j = i + 1; j < t; j++)
+                {
+                    if (n[j] < n[m])
+                    {
+                        m = j; // mniejszy element
+                    }
+                }
+
+
+                // Zamiana mniejszy element z elementem na pozycji i
+                if (m != i)
+                {
+                    int tmp = n[i];
+                    n[i] = n[m];
+                    n[m] = tmp;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", n));
+
+        };
+
+
+        //  Zasada działania algorytmu:
+        //  1. Lista elementów jest podzielona na dwie części – posortowaną i nieposortowaną.Na
+        //  początku posortowana część składa się tylko z pierwszego elementu.
+        //  2. Algorytm bierze pierwszy element z nieposortowanej części i wstawia go w odpowiednie
+        //  miejsce w posortowanej części.
+        //  3. Powtarza ten proces dla każdego kolejnego elementu z nieposortowanej części, aż cała lista
+        //  zostanie posortowana.
+        //  4. Proces kończy się, gdy wszystkie elementy znajdują się w posortowanej części.
+        //  NIE STOSUJEMY ŻADNYCH METOD SORTUJĄCYCH ELEMENTY W TABLICY!
+
+        static Action<int[]> Zad9 = (n) =>
+        {
+            Console.WriteLine($"\nZadanie 9 \n");
+
+            int t = n.Length;
+
+            for (int i = 1; i < t; i++)
+            {
+                int c = n[i]; // do wstawienia
+                int j = i - 1;
+
+                while(j >= 0 && n[j] > c)
+                {
+                    n[j + 1] = n[j]; // przesuwamy wiekszy element w prawo
+                    j--; // do poprzedniego elementu
+                }
+
+
+                // wstawienie odpowiednie miejsce
+                n[j + 1] = c;
+            }
+
+            Console.WriteLine(string.Join(", ", n));
+        };
 
         static void Main(string[] args)
         {
-            Zad1(59);
-            Zad2(75, 50);
-            Zad3("TEKSTR", 3);
+            Zad1(5);
+
+            Zad2(25,5);
+
+            Zad3("Tekst", 3);
+
             Zad4(10);
-            Zad5(60);
-            Zad6();
-            // Zadanie 7
-            int[] n = { 2, 5, 6, 7, 8 };
-            foreach(int i in n)
-            {
-                Console.Write($"{i} ");
-            }
+
+            Zad5(64);
+
+            int[] n = { 12, 5, 9, 20, 4 };
+
+            Zad6(n);
+            Zad7(n);
+            Zad8(n);
+            Zad9(n);
         }
 
     }
