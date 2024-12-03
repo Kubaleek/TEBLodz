e03717a1ebaa7a8be05668587440292c886bac08
Kuba Król <kuba.krol.ldz@gmail.com>
2024-12-03 16:00:12 +0100

    .

--- TworzenieWebowych-03.12.2024/main.js
+++ TworzenieWebowych-03.12.2024/main.js
@@ -6,6 +6,7 @@ function wylosuj(){
     let max = parseInt(document.getElementById('input3').value);
 
     for(let i=0; i < a; i++){
-        alert(Math.floor(Math.random() * (min - max) + max));
+        alert(Math.floor(Math.random() * (max - min) + min));
+        // alert(Math.floor(Math.random() * (min - max) + max));
     }
 }
