056132f39cc65d5556b9eafd39f33c675fb1a389
Kuba Król <kuba.krol.ldz@gmail.com>
2025-01-03 08:59:32 +0100

    .

---
+++ CalculatorMaui/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 340;
+            const int newHeight = 540;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
+    }
+}
